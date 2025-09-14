44cf1ea4fcaa94576e8ffb58abce90fceb2889e9
Your Name <you@example.com>
2025-09-14 19:01:55 +0300

    Знакомство с PageObject

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -5,25 +5,13 @@ from pages.registration_page import RegistrationPage
 
 @pytest.mark.registration
 @pytest.mark.regression
-@pytest.mark.parametrize(
-    "registration_data",
-    [
-        ("username", "user@gmail.com", "password"),
-        ("new_username", "user@gmail.com", "password")
-    ],
-    ids = [
-        "Valid registration data",
-        "Valid registration data with different username"
-    ]
-)
-def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, registration_data):
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    user_name, email, password = registration_data
 
     registration_page.fill_registration_form(
-        username=user_name,
-        email=email,
-        password=password
+        username="username",
+        email="user@gmail.com",
+        password="password"
     )
 
     registration_page.click_registration_button()
