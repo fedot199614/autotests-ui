1ef9f54b3138057809c128ee2e23c1d8b9241340
f.diulgher <fd@avantajprim.com>
2025-09-15 16:20:26 +0300

    Практикуемся в реализации PageObject

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,5 +1,7 @@
 import pytest
 from playwright.sync_api import Page
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 from pages.dashboard_page import DashboardPage
 from pages.registration_page import RegistrationPage
 
@@ -10,3 +12,11 @@ def registration_page(chromium_page: Page) -> RegistrationPage:
 @pytest.fixture(scope="function")
 def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
+
+@pytest.fixture
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
+
+@pytest.fixture
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -17,4 +17,4 @@ def test_successful_registration(registration_page: RegistrationPage, dashboard_
     registration_page.click_registration_button()
 
     dashboard_page.wait_for_load()
-    dashboard_page.check_dashboard_title()
+    dashboard_page.check_visible_dashboard_title()
